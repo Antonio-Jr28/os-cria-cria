6debf555265e77872e716f8b4edfa5047b154718
AntonioJr-28 <goncalvesantonio-jr@gmail.com>
2023-12-07 23:25:37 -0300

    atualizando

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "./components/navbar/navbar.components";
 export const App = () => {
   return (
     <section className="bg-black">
-      <Navbar />
+      <Navbar redesItems={[]} data={[]} />
     </section>
   );
 };
