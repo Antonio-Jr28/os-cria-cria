68affbbd4bc64fbf680f12535f943197c611112b
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2024-01-09 19:50:45 -0300

    finalizando carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { Carousel } from "./components/carousel/carousel-component";
 import { Navbar } from "./components/navbar/navbar.components";
 
+import Background from "../src/assets/banners/banner-1.png";
+import BackgroundTwo from "../src/assets/banners/banner-2.png";
+
 export const App = () => {
+   const images = [Background, BackgroundTwo];
+
   return (
     <>
       <section className="bg-black">
         <Navbar redesItems={[]} data={[]} />
       </section>
-      <section className="bg-gray-500 h-[500px]">sessão 1</section>
-      <section className="bg-gray-700 h-[500px]">sessão 2</section>
+      <section className="bg-gray-500 h-[500px]"><Carousel images={images} /></section>
     </>
   );
 };
