e82ea13757c9dfd9855eea6c6ee14366d70e95eb
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2024-01-09 22:36:22 -0300

    adicionando outra opcao de fonte

--- src/components/services/section-services.component.tsx
+++ src/components/services/section-services.component.tsx
@@ -1,7 +1,7 @@
 export const ServicesSection = () => {
   return (
     <div className="pl-44 pt-10">
-      <h1 className="text-don text-8xl text-white">Serviços</h1>
+      <h1 className="text-bombing text-8xl text-white">Serviços</h1>
       <ol className="grid md:grid-cols-2 gap-4 text-3xl pl-44 list-disc list-inside text-white pt-20">
         <li>Sites</li>
         <li>Banner</li>
