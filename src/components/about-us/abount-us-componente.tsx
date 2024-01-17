5dc303a00b365b226ac2067722ac9b8f895aee02
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2024-01-17 16:38:27 -0300

    finalizando parte um do quem somos

--- src/components/about-us/abount-us-componente.tsx
+++ src/components/about-us/abount-us-componente.tsx
@@ -1,7 +1,7 @@
 import { Divider } from '../ divider/divider.component'
-import Logo from '../../assets/icon/logo-desktop.svg'
 import { aboutUsStrings } from './about-us.strings'
 import Isabelly from '../../assets/img/isabelly-about.svg'
+import Antonio from '../../assets/img/antonio-about.svg'
 
 export const AboutUs = () => {
   return (
@@ -10,32 +10,44 @@ export const AboutUs = () => {
         <h1 className="text-bombing text-6xl md:text-8xl mt-5">
           {aboutUsStrings.title}
         </h1>
-        <img
-          className="w-[200px] h-[200px]"
-          src={Isabelly}
-          alt="Foto da coceo da empresa"
-        />
-        <h1>{aboutUsStrings.coCeo}</h1>
-        <p>{aboutUsStrings.descriptionCoCeo} </p>
-
-        <img src={Logo} />
-        <h1>{aboutUsStrings.ceo}</h1>
-        <p>{aboutUsStrings.descriptionCeo}</p>
-      </div>
-
-      <div className="h-[100px]" />
-
-      <div className="flex flex-col items-center w-[350px] md:w-[400px]">
-        <div>
-          <h1 className="text-bombing text-6xl md:text-8xl mt-5 text-white">
-            {aboutUsStrings.brand}
-          </h1>
+        <div className="flex flex-col md:flex-row items-center pl-5">
+          <img
+            className="w-[200px] h-[200px]"
+            src={Isabelly}
+            alt="Foto da coceo da empresa"
+          />
+          <div>
+            <h1 className="text-2xl">{aboutUsStrings.coCeo}</h1>
+            <p className="mt-4">{aboutUsStrings.descriptionCoCeo} </p>
+          </div>
         </div>
-        <p className="text-white text-center text-lg font-bold mt-20">
-          {aboutUsStrings.description}
-        </p>
+
+        <div className="flex flex-col md:flex-row-reverse pl-5 items-center">
+          <img
+            className="w-[200px] h-[200px]"
+            src={Antonio}
+            alt="Foto da coceo da empresa"
+          />
+          <div>
+            <h1 className="text-2xl">{aboutUsStrings.ceo}</h1>
+            <p className="mt-4">{aboutUsStrings.descriptionCeo}</p>
+          </div>
+        </div>
       </div>
-      <Divider />
+
+      <div className="h-[80px]" />
+
+      <div className="flex flex-col items-center w-[350px] md:w-[500px] pt-36">
+        <h1 className="text-bombing text-6xl md:text-8xl mt-5 text-white">
+          {aboutUsStrings.brand}
+        </h1>
+        <div className="w-[400px]">
+          <p className="text-white text-center text-lg font-bold ">
+            {aboutUsStrings.description}
+          </p>
+        </div>
+        <Divider />
+      </div>
     </div>
   )
 }
--- src/components/about-us/about-us.strings.ts
+++ src/components/about-us/about-us.strings.ts
@@ -1,9 +1,10 @@
 export const aboutUsStrings = {
   title: 'Quem Somos',
   coCeo: 'Isabelly Vitorino',
-  descriptionCoCeo: '',
+  descriptionCoCeo:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin',
   ceo: 'Antonio Carlos',
-  descriptionCeo: '',
+  descriptionCeo: 'Programador com um pé em designer.',
   brand: 'Os Cria Cria',
   description:
     ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin    vestibulum erat ut metus dictum, vel hendrerit justo bibendum. Ut ac urna nec risus tincidunt convallis. Vestibulum ante ipsum primis infaucibus orci luctus et ultrices posuere cubilia Curae; Integercommodo urna eget facilisis vulputate. Curabitur laoreet turpis at augue facilisis, nec varius justo malesuada. Sed in risus ut ipsumhendrerit tristique. Sed interdum vestibulum enim, id aliquam est lacinia ac. Duis volutpat orci eget tortor fermentum, eget mattis sapien commodo. In hac habitasse',
