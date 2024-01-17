5016c7f476913e2b92508c750e9f1e4e033e4809
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2024-01-17 17:07:39 -0300

    finalizando cor do background

--- src/components/about-us/abount-us-componente.tsx
+++ src/components/about-us/abount-us-componente.tsx
@@ -7,7 +7,7 @@ export const AboutUs = () => {
   return (
     <div className="flex flex-col md:flex-row items-center md:justify-around">
       <div className="flex flex-col items-center w-[350px] md:w-[500px]">
-        <h1 className="text-bombing text-6xl md:text-8xl mt-5">
+        <h1 className="text-bombing text-6xl md:text-8xl mt-5 text-white">
           {aboutUsStrings.title}
         </h1>
         <div className="flex flex-col md:flex-row items-center pl-5">
@@ -17,8 +17,10 @@ export const AboutUs = () => {
             alt="Foto da coceo da empresa"
           />
           <div>
-            <h1 className="text-2xl">{aboutUsStrings.coCeo}</h1>
-            <p className="mt-4">{aboutUsStrings.descriptionCoCeo} </p>
+            <h1 className="text-2xl text-white">{aboutUsStrings.coCeo}</h1>
+            <p className="mt-4 text-white">
+              {aboutUsStrings.descriptionCoCeo}{' '}
+            </p>
           </div>
         </div>
 
@@ -29,8 +31,8 @@ export const AboutUs = () => {
             alt="Foto da coceo da empresa"
           />
           <div>
-            <h1 className="text-2xl">{aboutUsStrings.ceo}</h1>
-            <p className="mt-4">{aboutUsStrings.descriptionCeo}</p>
+            <h1 className="text-2xl text-white">{aboutUsStrings.ceo}</h1>
+            <p className="mt-4 text-white">{aboutUsStrings.descriptionCeo}</p>
           </div>
         </div>
       </div>
--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -9,49 +9,53 @@ interface FooterProps {
 
 export const Footer: React.FC<FooterProps> = ({ services, medias }) => {
   return (
-    <div className="flex flex-col pl-4 pr-4 md:flex-row md:justify-around text-center backFooterColor h-[870px] md:h-[320px] w-[1000px] rounded-t-[50px]">
-      <div>
-        <h2 className="text-bombing text-5xl text-center mt-8">Os Cria Cria</h2>
-        <p className="w-auto md:w-[350px] mt-1">
-          Empresa criada para o desenvolvimento e crescimento de pequenas
-          empresas para grandes negócios.
-        </p>
-        <p className="text-black text-lg mt-8">
-          <i className="fas fa-phone mr-4 text-black"></i>
-          11 - 95427-7459
-        </p>
-        <p className="text-black text-lg mt-4">
-          <i className="fas fa-envelope mr-4 text-black"></i>
-          oscria.cria21@gmail.com
-        </p>
-        <p className="text-black text-lg mt-4">
-          <i className="fas fa-map-marker-alt mr-4 text-black"></i>
-          Rua: Montanhas, 61 Itaquera/SP
-        </p>
+    <>
+      <div className="flex flex-col pl-4 pr-4 md:flex-row md:justify-around text-center backFooterColor h-[870px] md:h-[320px] w-[1000px] rounded-t-[50px] mt-12">
+        <div>
+          <h2 className="text-bombing text-5xl text-center mt-8">
+            Os Cria Cria
+          </h2>
+          <p className="w-auto md:w-[350px] mt-1">
+            Empresa criada para o desenvolvimento e crescimento de pequenas
+            empresas para grandes negócios.
+          </p>
+          <p className="text-black text-lg mt-8">
+            <i className="fas fa-phone mr-4 text-black"></i>
+            11 - 95427-7459
+          </p>
+          <p className="text-black text-lg mt-4">
+            <i className="fas fa-envelope mr-4 text-black"></i>
+            oscria.cria21@gmail.com
+          </p>
+          <p className="text-black text-lg mt-4">
+            <i className="fas fa-map-marker-alt mr-4 text-black"></i>
+            Rua: Montanhas, 61 Itaquera/SP
+          </p>
+        </div>
+
+        <div>
+          <h2 className=" text-4xl mt-8">Serviços</h2>
+
+          <ul className="mt-8">
+            {services?.map((service, index) => (
+              <li key={index} className="text-black">
+                {service.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h2 className=" text-4xl mt-8">Redes Sociais</h2>
+
+          <ul className="mt-8">
+            {medias.map((rede, index) => (
+              <li key={index}>{rede}</li>
+            ))}
+          </ul>
+          <img className="pl-[80px] md:pl-[0]" src={Logo} alt="logo" />
+        </div>
       </div>
-
-      <div>
-        <h2 className=" text-4xl mt-8">Serviços</h2>
-
-        <ul className="mt-8">
-          {services?.map((service, index) => (
-            <li key={index} className="text-black">
-              {service.name}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div>
-        <h2 className=" text-4xl mt-8">Redes Sociais</h2>
-
-        <ul className="mt-8">
-          {medias.map((rede, index) => (
-            <li key={index}>{rede}</li>
-          ))}
-        </ul>
-        <img className="pl-[80px] md:pl-[0]" src={Logo} alt="logo" />
-      </div>
-    </div>
+    </>
   )
 }
