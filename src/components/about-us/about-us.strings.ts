9d1e932b61b83001b24845be6f7a3aea1a02fc50
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2024-01-20 13:31:07 -0300

    atualizando bografia

--- src/components/about-us/about-us.strings.ts
+++ src/components/about-us/about-us.strings.ts
@@ -2,10 +2,10 @@ export const aboutUsStrings = {
   title: 'Quem Somos',
   coCeo: 'Isabelly Vitorino',
   descriptionCoCeo:
-    'Programador experiente em UI/UX, apaixonado por criar interfaces intuitivas e atraentes. Minha jornada no mundo da programação é impulsionada pela curiosidade incessante, sempre buscando inovações e soluções criativas para aprimorar a experiência do usuário.',
+    'Programador experiente em UI/UX, apaixonado por criar interfaces intuitivas. Minha jornada no mundo da programação é impulsionada pela curiosidade, sempre buscando inovações e soluções criativas para aprimorar a experiência do usuário.',
   ceo: 'Antonio Carlos',
   descriptionCeo:
-    'Programador experiente em UI/UX, apaixonado por criar interfaces intuitivas e atraentes. Minha jornada no mundo da programação é impulsionada pela curiosidade incessante, sempre buscando inovações e soluções criativas para aprimorar a experiência do usuário.',
+    'Programador experiente em UI/UX, apaixonado por criar interfaces intuitivas. Minha jornada no mundo da programação é impulsionada pela curiosidade, sempre buscando inovações e soluções criativas para aprimorar a experiência do usuário.',
   brand: 'Os Cria Cria',
   description:
     ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin    vestibulum erat ut metus dictum, vel hendrerit justo bibendum. Ut ac urna nec risus tincidunt convallis. Vestibulum ante ipsum primis infaucibus orci luctus et ultrices posuere cubilia Curae; Integercommodo urna eget facilisis vulputate. Curabitur laoreet turpis at augue facilisis, nec varius justo malesuada. Sed in risus ut ipsumhendrerit tristique. Sed interdum vestibulum enim, id aliquam est lacinia ac. Duis volutpat orci eget tortor fermentum, eget mattis sapien commodo. In hac habitasse',
